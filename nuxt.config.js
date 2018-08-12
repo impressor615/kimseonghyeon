7c30c6cfece1a7b4d754218fe833739b821c85b7
seonghyeon <hyeon@apartmentary.com>
2018-08-12 16:41:35 +0900

    add favicon, robots.txt

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,13 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Seonghyeon Kim Wkebsite' }
     ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/my-favicon.ico',
+      },
+    ],
   },
   css: [
     '~/assets/stylesheets/global.scss',
