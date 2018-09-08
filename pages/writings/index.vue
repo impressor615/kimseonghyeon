2ca1b7f59d88bbfd414cb48b81b8a7cb20ca17b6
seonghyeon <hyeon@apartmentary.com>
2018-09-08 23:23:05 +0900

    add /writings page

---
+++ pages/writings/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <article>
+    <list-layout>
+      <div slot="title">WRITINGS</div>
+      <div class="no-item">아직 등록된 글이 없습니다.</div>
+    </list-layout>
+  </article>
+</template>
+
+<script>
+import ListLayout from '~/components/ListLayout';
+
+export default {
+  components: {
+    ListLayout,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.no-item {
+  margin: 3rem;
+
+  font-size: 1.1rem;
+}
+</style>
