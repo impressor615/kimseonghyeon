cf066f7255e1db773fa0590ac9fc49a1fac69647
seonghyeon <hyeon@apartmentary.com>
2018-09-08 23:25:14 +0900

    improve project's list page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,31 +1,29 @@
 <template>
   <article>
-    <div class="project-list">
-      <div class="title">PROJECTS</div>
-      <div class="list">
-        <project-item
-          title="MY BLOG"
-          to="/projects/my-blog"
-          :desc="myblog"
-        >
-          <div class="icon-list">
-            <round-icon type="javascript" />
-            <round-icon type="nuxt" />
-          </div>
-        </project-item>
-        <project-item
-          title="REACT SSR"
-          to="/projects/react-ssr-boilerplate"
-          :desc="reactSSR"
-        >
-          <div class="icon-list">
-            <round-icon type="express" />
-            <round-icon type="react" />
-            <round-icon type="redux" />
-          </div>
-        </project-item>
-      </div>
-    </div>
+    <list-layout>
+      <div slot="title">PROJECTS</div>
+      <project-item
+        title="MY BLOG"
+        to="/projects/my-blog"
+        :desc="myblog"
+      >
+        <div class="icon-list">
+          <round-icon type="javascript" />
+          <round-icon type="nuxt" />
+        </div>
+      </project-item>
+      <project-item
+        title="REACT SSR"
+        to="/projects/react-ssr-boilerplate"
+        :desc="reactSSR"
+      >
+        <div class="icon-list">
+          <round-icon type="express" />
+          <round-icon type="react" />
+          <round-icon type="redux" />
+        </div>
+      </project-item>
+    </list-layout>
   </article>
 </template>
 
@@ -33,11 +31,13 @@
 import { mapGetters } from 'vuex';
 import ProjectItem from '~/components/ProjectItem';
 import RoundIcon from '~/components/RoundIcon';
+import ListLayout from '~/components/ListLayout';
 
 export default {
   components: {
     ProjectItem,
     RoundIcon,
+    ListLayout,
   },
   computed: {
     myblog() {
@@ -50,7 +50,6 @@ export default {
       const english = 'I built React SSR Boilerplate using React and Express';
       return this.language === 'ko' ? korean : english;
     },
-
     ...mapGetters([
       'language',
     ]),
@@ -61,29 +60,12 @@ export default {
 <style lang="scss" scoped>
 @import '~/assets/stylesheets/_variables.scss';
 
-.project-list {
-  display: flex;
-  flex-direction: column;
-  padding: 3rem;
-}
-
-.project-list .title {
-  font-size: 2.5rem;
-  font-weight: bold;
-}
-
-.project-list .list {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-.project-list .icon-list {
+.icon-list {
   display: flex;
   justify-content: center;
 }
 
-.project-list .icon-list > div {
+.icon-list > div {
   margin: .2rem;
 }
 </style>
