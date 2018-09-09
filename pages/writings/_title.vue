7b958d1046a3cf96d3096fe02667f6622e297c69
seonghyeon <hyeon@apartmentary.com>
2018-09-09 17:53:51 +0900

    add title, date, keywords to /writings/:title page

--- pages/writings/_title.vue
+++ pages/writings/_title.vue
@@ -1,12 +1,22 @@
 <template>
   <article>
-    <div class="writing-container" v-html="mdHtml" />
+    <div class="writing-container">
+      <div class="title">{{ title }}</div>
+      <div class="date">{{ formattedDate }}</div>
+      <ul class="keywords">
+        <li v-for="keyword in keywords" :key="keyword">
+          {{ keyword }}
+        </li>
+      </ul>
+      <div v-html="mdHtml" class="content"></div>
+    </div>
   </article>
 </template>
 
 <script>
 import marked from 'marked';
 import WRTINGS from '~/constants/writings';
+import { formattedDate } from '~/utils/dateUtils';
 
 export default {
   middleware: ['checkParams'],
@@ -35,6 +45,9 @@ export default {
     mdHtml() {
       return this.writing ? marked(this.writing) : '';
     },
+    formattedDate() {
+      return formattedDate(this.published_date);
+    }
   },
 };
 </script>
@@ -44,6 +57,43 @@ export default {
 
 .writing-container {
   padding: 3rem;
+}
+
+.title {
+  font-size: 2rem;
+  font-weight: $font-bold;
+
+  margin-bottom: 1.5rem;
+}
+
+.date {
+  font-weight: $font-thin;
+
+  margin-bottom: 1rem;
+}
+
+.keywords {
+  display: flex;
+  flex-wrap: wrap;
+  width: 50%;
+
+  margin-top: 1rem;
+}
+
+.keywords li {
+  margin: 0 .5rem .5rem 0;
+  padding: .3rem .6rem;
+
+  background: gray;
+  border-radius: 25px;
+
+  font-size: .8rem;
+  font-weight: $font-regular;
+  color: black;
+}
+
+.content {
+  margin-top: 2rem;
 }
 
 @media only screen and (max-width: $screen-md) {
