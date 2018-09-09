2710bcbb43a2ec44737717f10ee9941ec4802b60
seonghyeon <hyeon@apartmentary.com>
2018-09-09 16:57:32 +0900

    add BlogItem to /wrtings page

--- pages/writings/index.vue
+++ pages/writings/index.vue
@@ -2,25 +2,43 @@
   <article>
     <list-layout>
       <div slot="title">WRITINGS</div>
-      <div class="no-item">아직 등록된 글이 없습니다.</div>
+      <blog-item
+        v-for="blog in list"
+        :key="blog.title"
+        :title="blog.title"
+        :date="blog.published_date"
+        :keywords="blog.keywords"
+        :to="`/writings/${blog.key}`"
+      />
+      <div v-if="!list.length" class="no-items">
+        아직 등록 된 글이 없습니다.
+      </div>
     </list-layout>
   </article>
 </template>
 
 <script>
 import ListLayout from '~/components/ListLayout';
+import BlogItem from '~/components/BlogItem';
+import WRITINGS from '~/constants/writings';
 
 export default {
   components: {
     ListLayout,
   },
+  data: function() {
+    const { list } = WRITINGS;
+    return {
+      list,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.no-item {
-  margin: 3rem;
+.no-items {
+  margin: 2rem;
 
-  font-size: 1.1rem;
+  font-size: 1.2rem;
 }
 </style>
