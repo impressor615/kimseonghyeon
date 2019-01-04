1c1ebda69f5782e37ea7d35dd1483753ed97c6bf
seonghyeon <hyeon@apartmentary.com>
2019-01-04 11:45:48 +0900

    add working task

--- constants/writings.js
+++ constants/writings.js
@@ -1,3 +1,11 @@
 export default {
-  list: [],
+  list: [
+    {
+      key: 'how-to-be-everything',
+      title: '모든 것이 되는 법 - How To Be Everything',
+      description: '관심있는 것이 하나가 아닌 제 자신을 위해 읽기 시작했고, 책 내용을 정리 해보았습니다.',
+      keywords: ['제너럴리스트', '독서'],
+      published_date: '2018-09-09',
+    },
+  ],
 };
